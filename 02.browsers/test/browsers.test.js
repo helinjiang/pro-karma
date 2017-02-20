1d513249e9ba53991f6597a72e898dfd094194f4
helinjiang <me@helinjiang.com>
2017-02-20 10:51:52 +0800

    add 02.browsers示例

--- 02.browsers/test/browsers.test.js
+++ 02.browsers/test/browsers.test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 
 describe('测试浏览器兼容', function () {
-    it('测试 window.chrome 属性的支持', function () {
+    it('测试浏览器对 window.chrome 属性的支持', function () {
         // window.chrome是 Chrome 平台中独有的
         // https://developer.chrome.com/apps/api_index
         expect(window.chrome).to.not.be.undefined;
